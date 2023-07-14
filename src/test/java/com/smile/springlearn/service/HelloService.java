6092d151fee07478d2fa211954b0e974f9206e60
wangyajun <wangyajun@loopin.group>
2023-07-14 16:30:25 +0800

    使用dom4j 解析xml & 加入Aware 让继承的bean感知对应的aware

--- src/test/java/com/smile/springlearn/service/HelloService.java
+++ src/test/java/com/smile/springlearn/service/HelloService.java
@@ -1,10 +1,39 @@
 package com.smile.springlearn.service;
 
-public class HelloService {
+import com.smile.springlearn.beans.BeansException;
+import com.smile.springlearn.beans.factory.BeanFactory;
+import com.smile.springlearn.beans.factory.BeanFactoryAware;
+import com.smile.springlearn.context.ApplicationContext;
+import com.smile.springlearn.context.ApplicationContextAware;
+
+public class HelloService implements ApplicationContextAware, BeanFactoryAware {
+
+    private BeanFactory beanFactory;
+
+    private ApplicationContext applicationContext;
 
     public String sayHello(){
         System.out.println("hello...");
         return "Hello";
     }
 
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+    public BeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
 }
